8d8e0e3effcb023b0bb0eceda72eb91502441211
Brandon Nozaki Miller <brandon@diginow.it>
2022-01-22 10:29:24 -0800

    started examples

---
+++ examples/GET/basic.js
@@ -0,0 +1,29 @@
+/*
+if running on node,
+
+import fetch from 'node-fetch';
+*/
+
+const TRANSACTION_ID='4d03';
+const BASE = 'https://api.stage.getocra.com';
+const HEADERS = {
+    'content-type': 'application/json',
+    'Authorization': 'YOUR_API_KEY_HERE'
+};
+const METHOD='GET';
+
+const response = await fetch(
+    `${BASE}/transaction/${TRANSACTION_ID}`, 
+    {
+        headers: HEADERS,
+        method: METHOD,
+    }
+);
+
+/*
+
+for response payload see 
+
+https://github.com/GetOcra/ocra-public-api-documentation/blob/main/Transaction.md#example-get-success-response
+
+*/
---
+++ examples/POST/basic.js
@@ -0,0 +1,44 @@
+/*
+if running on node,
+
+import fetch from 'node-fetch';
+*/
+
+const TRANSACTION_ID='4d03';
+const BASE = 'https://api.stage.getocra.com';
+const HEADERS = {
+    'content-type': 'application/json',
+    'Authorization': 'YOUR_API_KEY_HERE'
+};
+const METHOD='POST';
+
+const response = await fetch(
+    `${BASE}/transaction/${TRANSACTION_ID}`, 
+    {
+        headers: HEADERS,
+        method: METHOD,
+        body:JSON.stringify(
+            {
+                transactionID  : TRANSACTION_ID,
+                transactionDate: '2021-10-05T14:48:00.000Z',
+                locationId     : '4557',
+                startDate      : '2021-10-05T15:00:00.000Z', 
+                endDate        : '2021-10-05T16:00:00.000Z',
+                licensePlate   : 'AAA-111',
+                grossRevenue   : 23.01,
+                netRevenue     : 20.25,
+                productType    : 'covered',
+                vertical       : 'airport',
+                barcode        : '123556469764513'
+            }
+        )
+    }
+);
+
+/*
+
+for response payload see 
+
+https://github.com/GetOcra/ocra-public-api-documentation/blob/main/Transaction.md#example-delete-success-response
+
+*/
