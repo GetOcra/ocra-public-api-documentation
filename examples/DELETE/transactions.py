4c59fd78d05a247001057442c430d93d2adfb1e5
Brandon Nozaki Miller <brandon@diginow.it>
2022-01-25 10:31:20 -0800

    python delete example

---
+++ examples/DELETE/transactions.py
@@ -0,0 +1,20 @@
+import requests;
+
+TRANSACTION_ID='4d03'
+BASE = 'https://api.stage.getocra.com'
+HEADERS = {
+    'content-type': 'application/json',
+    'Authorization': 'YOUR_API_KEY_HERE'
+}
+
+response = requests.delete(
+    f'{BASE}/transaction/{TRANSACTION_ID}', 
+    headers=HEADERS
+)
+
+#debug only
+print(response)
+print(response.json())
+
+## for response payload see 
+## https://github.com/GetOcra/ocra-public-api-documentation/blob/main/Transaction.md#example-get-success-response
