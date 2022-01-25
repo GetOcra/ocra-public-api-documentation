b7d8bef6fa2666cf857519eb2516277a6d153151
Brandon Nozaki Miller <brandon@diginow.it>
2022-01-25 10:35:36 -0800

    python POST example

---
+++ examples/POST/transactions.py
@@ -0,0 +1,36 @@
+import requests;
+import json;
+
+TRANSACTION_ID='4d03'
+BASE = 'https://api.stage.getocra.com'
+HEADERS = {
+    'content-type': 'application/json',
+    'Authorization': 'YOUR_API_KEY_HERE'
+}
+
+response = requests.delete(
+    f'{BASE}/transaction/{TRANSACTION_ID}', 
+    headers=HEADERS,
+    data=json.dumps(
+        {
+            transactionID  : TRANSACTION_ID,
+            transactionDate: '2021-10-05T14:48:00.000Z',
+            locationId     : '4557',
+            startDate      : '2021-10-05T15:00:00.000Z', 
+            endDate        : '2021-10-05T16:00:00.000Z',
+            licensePlate   : 'AAA-111',
+            grossRevenue   : 23.01,
+            netRevenue     : 20.25,
+            productType    : 'covered',
+            vertical       : 'airport',
+            barcode        : '123556469764513'
+        }
+    )
+)
+
+#debug only
+print(response)
+print(response.json())
+
+## for response payload see 
+## https://github.com/GetOcra/ocra-public-api-documentation/blob/main/Transaction.md#example-get-success-response
